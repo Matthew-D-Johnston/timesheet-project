89fcc618c131b6ab5087acba1a9726d668e4f396
Matthew Johnston <mattdjohnston92@gmail.com>
2020-08-29 21:17:59 -0400

    Add updated project files and new views files.

--- timesheet.rb
+++ timesheet.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/content_for"
 require "tilt/erubis"
+require "pry"
 
 require_relative "database_persistence"
 
@@ -32,7 +33,23 @@ get "/timesheet" do
 end
 
 get "/input_time" do
+  erb :time_input
+end
 
+get "/cc_credit" do
+  erb :cc_credit
+end
+
+get "/flat_rate" do
+  erb :flat_rate
+end
+
+get "/hourly_rate" do
+  erb :hourly_rate
+end
+
+get "/data_verification" do
+  erb :data_verification
 end
 
 get "/create_invoice" do
@@ -41,4 +58,41 @@ end
 
 get "/create_report" do
 
+end
+
+post "/time_input" do
+  session[:article_name] = params[:article_name]
+  session[:url] = params[:url]
+  session[:word_count] = params[:word_count]
+
+  session[:day] = params[:day]
+  session[:month] = params[:month]
+  session[:year] = params[:year]
+
+  if params[:cc_credit]
+    redirect "/cc_credit"
+  elsif params[:flat_rate]
+    redirect "/flat_rate"
+  else
+    redirect "/hourly_rate"
+  end
+end
+
+post "/cc_credit" do
+  session[:cc_credits] = params[:credits]
+
+  redirect "/data_verification"
+end
+
+post "/flat_rate" do
+  session[:flat_rate] = params[:flat_rate]
+
+  redirect "/data_verification"
+end
+
+post "/hourly_rate" do
+  session[:hourly_rate] = params[:hourly_rate]
+  session[:hours] = params[:hours]
+
+  redirect "/data_verification"
 end
